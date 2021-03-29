f787d25e75ce0c2181ef654491b95489398d7fd8
jgoueslard <76858231+jgoueslard@users.noreply.github.com>
2021-03-29 11:04:58 +0200

    yeye

--- test.py
+++ test.py
@@ -6,11 +6,3 @@ Created on Mon Mar 29 09:27:21 2021
 @author: maxinegravier
 """
 print("hello c'est maxine")
-print("test")
-a = 5
-b=4
-c = a+b 
-print(c)
-print(c+a)
-print("test test")
-print("dernier test")
