03ff0f6adb401e2c65e3985704224cb7d4031ee2
RedaPengam <71394086+RedaPengam@users.noreply.github.com>
2021-04-06 11:33:38 +0200

    Update class_Joueur.py

--- Collision Game/class_Joueur.py
+++ Collision Game/class_Joueur.py
@@ -1,9 +1,13 @@
 import pygame as pg
 
-class Joueur:
+class Joueur(pg.sprite.Sprite):
 
-    def __init__(pg.sprite.Sprite):
+    def __init__(self, numero):
         super().__init__()
         self.health = 100
         self.max_health = 100
         self.velocity = 5
+        self.numero = numero
+        self.image = pg.image.load('data/joueur{}.png'.format(self.numero))
+        self.image = pg.transform.scale(self.image, (100, 70))
+        self.rect = self.image.get_rect()
