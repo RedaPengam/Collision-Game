cc5d310ec7a81b1b8c8d79b58cbeefc1e8d31ecb
Max-Ine33 <maxinegravier@gmail.com>
2021-04-09 00:30:13 +0200

    Update class_Player.py

--- Collision Game/class_Player.py
+++ Collision Game/class_Player.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from class_Projectile import Projectile
 
 class Player(pg.sprite.Sprite):
 
@@ -8,11 +9,15 @@ class Player(pg.sprite.Sprite):
         self.max_health = 100
         self.attack = 10
         self.velocity = 5
+        self.all_projectiles=pg.sprite.Group()
         self.image = pg.image.load('data/joueur1.png')
         self.image = pg.transform.scale(self.image, (100, 70))
         self.rect = self.image.get_rect()
         self.rect.x = 20
         self.rect.y = 90
+        
+    def launch_projectile(self):
+        self.all_projectiles.add(Projectile(self))
 
     def move_up(self):
         self.rect.y -= self.velocity
