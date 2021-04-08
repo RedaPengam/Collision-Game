08fb1eabc35aaf6a773ed0e3f2f3a098d7069293
RedaPengam <71394086+RedaPengam@users.noreply.github.com>
2021-04-08 20:05:25 +0200

    Update class_Player.py

--- Collision Game/class_Player.py
+++ Collision Game/class_Player.py
@@ -7,7 +7,7 @@ class Player(pg.sprite.Sprite):
         self.health = 100
         self.max_health = 100
         self.attack = 10
-        self.velocity = 5
+        self.velocity = 1
         self.image = pg.image.load('data/joueur1.png')
         self.image = pg.transform.scale(self.image, (100, 70))
         self.rect = self.image.get_rect()
