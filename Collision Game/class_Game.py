87d31b43d68246a91a34340fd06ad3e630610c3c
clmntdhll <81157373+clmntdhll@users.noreply.github.com>
2021-04-12 09:32:07 +0200

    Update class_Game.py

--- Collision Game/class_Game.py
+++ Collision Game/class_Game.py
@@ -6,8 +6,11 @@ class Game:
 
     def __init__(self):
         # players creation
-        self.player1 = Player()
-        self.player2 = Player()
+        self.all_players = pg.sprite.Group()
+        self.player1 = Player(self)
+        self.player2 = Player(self)
+        self.all_players.add(self.player1)
+        self.all_players.add(self.player2)
         # corrects the player2 initial position
         self.player2.rect.x = 1160
         self.player2.rect.y = 400
@@ -16,11 +19,16 @@ class Game:
         # keys currently pressed
         self.pressed = {}
         # asteroids creation
+        self.all_asteroids =pg.sprite.Group()
         self.asteroid1 = Asteroid()        
         self.asteroid2 = Asteroid()
         self.asteroid3 = Asteroid()        
         self.asteroid4 = Asteroid()
-
+        self.all_asteroids.add(self.asteroid1, self.asteroid2, self.asteroid3, self.asteroid4)
+        
+    
+    def check_collision (self, sprite, group):
+        return pg.sprite.spritecollide(sprite, group, False, pg.sprite.collide_mask)
 
 
 
