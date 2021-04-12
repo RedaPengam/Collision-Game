8e21b6cc6e19501123a8912c7e0932ad2626a321
clmntdhll <81157373+clmntdhll@users.noreply.github.com>
2021-04-12 09:31:54 +0200

    Update class_Player.py

--- Collision Game/class_Player.py
+++ Collision Game/class_Player.py
@@ -3,8 +3,9 @@ from class_Projectile import Projectile
 
 class Player(pg.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self,game):
         super().__init__()
+        self.game = game
         self.health = 100
         self.max_health = 100
         self.attack = 10
@@ -16,11 +17,41 @@ class Player(pg.sprite.Sprite):
         self.rect.x = 20
         self.rect.y = 320
         
-    def launch_projectile(self):
+    def damage(self,amount):
+        #infliger les dégats
+        self.health-=amount
+        
+        #vérifier qu'il lui reste des points de vie 
+        if self.health <= 0:
+            #réappatraitre comme un nouveau monstre 
+            self.rect.y = 320 
+            self.health = self.max_health 
+    
+    def update_health_bar (self, surface):
+        # definir une couleur pour notre jauge de vie 
+        bar_color = (111, 210, 46)
+        
+        # definir couleur barre arrière plan 
+        back_bar_color = (60, 63, 60)
+        
+        # definir la position de la jauge de vie sa largeur et son épaisseur
+        bar_position = [self.rect.x , self.rect.y-20 , self.health, 5]
+        
+        # definir la position de la jauge de vie d'arrière plan sa largeur et son épaisseur
+        back_bar_position = [self.rect.x , self.rect.y-20 , self.max_health, 5]
+        
+        # dessiner la barre de vie
+        pg.draw.rect(surface, back_bar_color, back_bar_position)
+        pg.draw.rect(surface, bar_color, bar_position)
+        
+    
+        
+    def launch_projectile(self): 
         self.all_projectiles.add(Projectile(self))
 
     def move_up(self):
-        self.rect.y -= self.velocity
+        self.rect.y -= self.velocity 
 
     def move_down(self):
         self.rect.y += self.velocity
+       
