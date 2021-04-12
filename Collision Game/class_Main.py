572b0681b9cf8717667d106df40371187cfaae17
clmntdhll <81157373+clmntdhll@users.noreply.github.com>
2021-04-12 11:40:22 +0200

    Update class_Main.py

--- Collision Game/class_Main.py
+++ Collision Game/class_Main.py
@@ -100,10 +100,10 @@ while gameIsOn:
             game.pressed[event.key] = True
             
             if event.key == pg.K_SPACE:
-                game.player1.launch_projectile1()
+                game.player1.launch_projectile()
                 
             if event.key == pg.K_RETURN:
-                game.player2.launch_projectile2()
+                game.player2.launch_projectile()
                 
         elif event.type == pg.KEYUP:
             game.pressed[event.key] = False
